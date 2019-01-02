82b70d1f0d1f899d006e51a71fa39dd0d1e21f17
djpluki <djpluki@gmail.com>
2019-01-02 09:02:18 +0100

    setup vuex

---
+++ src/store/currentUser/currentUser.js
@@ -0,0 +1,16 @@
+const state = {
+  vuexTest: 'vuex-test',
+};
+
+const getters = {};
+
+const mutations = {};
+
+const actions = {};
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+};
