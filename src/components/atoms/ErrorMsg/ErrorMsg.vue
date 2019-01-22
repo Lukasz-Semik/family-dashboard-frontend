814d1f581f6e23d3f6859df346feaa0624670daf
djpluki <djpluki@gmail.com>
2019-01-22 20:57:38 +0100

    create ErrorMsg component

---
+++ src/components/atoms/ErrorMsg/ErrorMsg.vue
@@ -0,0 +1,21 @@
+<template>
+  <p :class="[$style['error-msg']]">{{ translatedText || $t(translationPath) }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    translationPath: {
+      type: String,
+      default: '',
+    },
+    translatedText: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+
+<style lang="scss" module src="./ErrorMsg.scss" />
