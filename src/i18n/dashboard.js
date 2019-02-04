4585e990d7da15355fb81554338a858b213a3caf
djpluki <djpluki@gmail.com>
2019-02-04 09:43:40 +0100

    add some respo on dashboard

--- src/i18n/dashboard.js
+++ src/i18n/dashboard.js
@@ -1,6 +1,6 @@
 export default {
-  welcome: 'Hi! {userName} :D',
-  welcomeNote: `It seems that you haven't created family dashboard yet. 
+  welcome: 'Hi {userName}!',
+  welcomeNote: `It seems that you haven't created family dashboard yet.
     You can ask someone to add you, or create own one.`,
   familyName: 'Family name',
   createFamily: 'Create family',
